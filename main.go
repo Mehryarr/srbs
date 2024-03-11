0c05eb85e463225b0637a9bcf5ec2ab592f65cde
Mehryarr <mehryarja@gmail.com>
2024-03-11 08:51:20 +0330

    adding cls function and sorting and printing city

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"strconv"
 )
 
@@ -16,9 +17,16 @@ type a struct {
 	sans   []string
 }
 
+func cls() {
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
 func main() {
 
 	var data []a
+	var city []string
 
 	file, err := os.Open("data.txt")
 	if err != nil {
@@ -59,5 +67,34 @@ func main() {
 		}
 		data = append(data, temp)
 	}
-	fmt.Println(data)
+	//fmt.Println(data)
+	sw := 0
+	for i := 0; i < len(data); i++ {
+		for j := 0; j < len(city); j++ {
+			if data[i].city == city[j] {
+				sw++
+				break
+			}
+		}
+		if sw == 0 {
+			city = append(city, data[i].city)
+		}
+		sw = 0
+	}
+
+	for i := 0; i < len(city)-1; i++ {
+		for j := 0; j < len(city)-i-1; j++ {
+			if city[j] > city[j+1] {
+				temp := city[j]
+				city[j] = city[j+1]
+				city[j+1] = temp
+			}
+		}
+	}
+
+	cls()
+	for i := 0; i < len(city); i++ {
+		fmt.Println(i+1, city[i])
+	}
+
 }
